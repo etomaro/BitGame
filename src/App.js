164d341a12c7af612e9a6eab9e94cd880ad5604d
etomaro <python.helloworld111@gmail.com>
2023-04-16 18:09:50 +0900

    make ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import React, { useState, useEffect } from 'react';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 // 4bit2進数と対応する10進数のオブジェクトを作成
 const bit4 = {
@@ -21,31 +29,114 @@ const bit4 = {
   '1110': 14,
   '1111': 15,
 }
-// bit4のキーを問題、値を答えとしたサイトを作成
-const bit4site = Object.keys(bit4).map((key) => {
-  return (
-    <div>
-      <p>{key}</p>
-      <p>{bit4[key]}</p>
-    </div>
-  )
-})
 
-// キーボードで入力して答える問題サイトを作成
-const bit4site2 = Object.keys(bit4).map((key) => {
+
+// 数値ボタンのスタイル
+const buttonStyle = {
+  width: "calc(100% / 5)",
+  height: "100px",
+  // 背景を薄水色にする
+  backgroundColor: "#e0ffff",
+  // ボーダーを黒色にする
+  border: "solid 1px #000000",
+  // ボタンの文字を黒にする
+  color: "#000000",
+};
+// okボタンのスタイル
+const okButtonStyle = {
+  width: "calc(100% / 7)",
+  height: "70px",
+  // ボーダーを黒色にする
+  border: "solid 1px #000000",
+  // ボタンの文字を黒にする
+  color: "#000000",
+  // 中央に配置する
+  margin: "auto",
+  // 上との間隔をあける
+  marginTop: "20px",
+};
+
+// inputのスタイル
+const inputStyle = {
+  // 背景を薄水色にする
+  backgroundColor: "#e0ffff",
+  // ボタンの文字を黒にする
+  color: "#000000",
+  height: "50px",
+  // フォントサイズを大きくする
+  fontSize: "30px",
+  // 真ん中に配置する
+  marginLeft: "7%",
+};
+
+
+// QandA
+const QandA = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  // state
+  const [seInNum, setInNum] = useState("");
+
+
+  // ボタンをクリックしたときの処理
+  const btnClick = (e) => {
+    setInNum(seInNum + e.target.textContent);
+  }
+  // inputの値を変更したときの処理
+  const inputChange = (e) => {
+    // TextFieldの値を取得
+    setInNum(e.target.value);
+  }
+  // 削除ボタンをクリックしたときの処理
+  const deleteClick = () => {
+    // seInNumの最後の文字を削除
+    setInNum(seInNum.slice(0, -1));
+  }
+  // okボタンをクリックしたときの処理
+  const okClick = () => {
+    // test
+
+  }
+  // seInNumの値を初期化する処理
+  const initInNum = () => {
+    setInNum("");
+  }
   return (
-    <div>
-      <p>{key}</p> 
-      <input type="text" />
-    </div>
+      <>
+        <Box>
+          <Box mb={3}>
+            {/* 入力 */}
+            <TextField onChange={inputChange} id="standard-basic" value={seInNum} style={inputStyle}/>
+            {/* 削除ボタン */}
+            <IconButton onClick={deleteClick} aria-label="delete" color="primary">
+              <DeleteIcon fontSize="large"/>
+            </IconButton>
+          </Box>
+          {/* 数値ボタン */}
+          <Box display="flex" flexWrap="wrap">
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>0</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>1</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>2</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>3</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>4</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>5</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>6</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>7</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>8</Button>
+            <Button variant="contained" style={buttonStyle} onClick={btnClick}>9</Button>
+          </Box>
+          {/* okボタン */}
+          <Button variant="contained" style={okButtonStyle}>ok</Button>
+        </Box>
+      </>
   )
-})
+}
 function App() {
   return (
-    // 背景が黒くなるので、App-headerの背景色を変更
     <div className="App">
       <header className="App-header">
-        {bit4site2}
+        <QandA />
       </header>
     </div>
   );
