36c720486e20d8fc20f3a060d890a760021f8f37
etomaro <python.helloworld111@gmail.com>
2023-06-17 17:44:17 +0900

    tmp

--- src/components/Record.js
+++ src/components/Record.js
@@ -10,9 +10,14 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import Checkbox from "@mui/material/Checkbox";
 import { pink } from '@mui/material/colors';
+import { useEffect } from 'react';
 
 
 export const Record = () => {
+
+    useEffect(() => {
+        console.log("レコードページ レンダリング")
+    }, []);
   
     return (
         <>
